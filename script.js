5a973e2e572466b7d210b44f13fa347e821da526
Izayah <izayahfuller3@gmail.com>
2025-02-19 15:51:14 -0500

    chanched imgs and added expanding

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ document.getElementById('enterButton').addEventListener('click', function() {
     console.log("testclick");
     this.style.opacity = '0';
     setTimeout(() => this.style.display = 'none', 500);
+    
     const image = document.getElementById('image');
+    const background= document.getElementById('image2');
     image.classList.add('hidden');
-    setTimeout(() => image.style.display = 'none', 1000);
+    background.classList.add('hidden');
+    setTimeout(() => {
+        image.style.display = 'none';
+        background.style.display = 'none';
+        window.location.href = "menu.html"; // Redirects to menu.html
+    }, 1000);
 });
